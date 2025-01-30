40c33add55a2046066ff96d64c1b29cbc8255554
russian_proger <sagitov.2806@mail.ru>
2025-01-30 14:50:19 +0300

    db(Person): make vk_id field unique

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-    vk_id = models.IntegerField()
+    vk_id = models.IntegerField(unique=True)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     photo = models.URLField()
