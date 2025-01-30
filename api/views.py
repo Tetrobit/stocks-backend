ee11e9dbd3852c957306f8c2f8313b0e36cf7ea7
russian_proger <sagitov.2806@mail.ru>
2025-01-30 14:50:45 +0300

    feat(auth): save profile information and return it to the client

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ import json
 import requests
 from django.http import HttpRequest, JsonResponse
 
+from .models import Person
+
 from .vkapi import account_get_profile_info
 
 def index(_request: HttpRequest):
@@ -14,8 +16,22 @@ def auth(request: HttpRequest):
     """ Auth user via VK """
     # requests.post()
     data = json.loads(request.body)
-    user_info = account_get_profile_info(data['access_token'])['response']
-    return JsonResponse({ 'ok': True, 'response': user_info })
+    response = account_get_profile_info(data['access_token'])['response']
+    person_info = {
+        'vk_id': response['id'],
+        'first_name': response['first_name'],
+        'last_name': response['last_name'],
+        'photo': response['photo_200']
+    }
+
+    person = Person.objects.filter(vk_id=response['id'])
+    if not person.exists():
+        person = Person.objects.create(**person_info)
+        person.save()
+    else:
+        person = person.first()
+
+    return JsonResponse({ 'ok': True, 'response': person_info })
 
 def check_auth(request: HttpRequest):
     """ Check auth """
