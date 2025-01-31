5473c74b28f60e2cd8cdbf6f48b80bbf11c1821a
russian_proger <sagitov.2806@mail.ru>
2025-01-31 18:34:12 +0300

    fix(auth.middleware): add error handling

--- api/middleware.py
+++ api/middleware.py
@@ -5,15 +5,18 @@ from .utils.jwt import verifyJwt
 class AuthMiddleware():
     def __init__(self, get_response):
         self.get_response = get_response
-    
+
     def __call__(self, request: HttpRequest):
         request.authorized = False
 
         access_token = request.COOKIES.get('access_token', None)
 
         if isinstance(access_token, str) and len(access_token) > 0:
-            user_info = verifyJwt(access_token)
-            print(user_info)
+            try:
+                user_info = verifyJwt(access_token)
+            except:
+                user_info = None
+
             if user_info:
                 request.authorized = True
                 request.user_id = user_info['id']
